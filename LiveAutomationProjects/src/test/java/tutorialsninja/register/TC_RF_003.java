43e3f19b2ef0619ca492b7114e7bd51c2026414a
goofcoder99 <55548446+Soumali-coder19@users.noreply.github.com>
2025-06-29 17:05:24 +0530

    TC_RF_004 uploaded

---
+++ LiveAutomationProjects/src/test/java/tutorialsninja/register/TC_RF_003.java
@@ -0,0 +1,65 @@
+package tutorialsninja.register;
+
+import java.time.Duration;
+import java.util.Date;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class TC_RF_003 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver  = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+		driver.get("https://tutorialsninja.com/demo/");
+		
+		driver.findElement(By.xpath("//span[text()='My Account']")).click();
+		driver.findElement(By.linkText("Register")).click();
+		driver.findElement(By.id("input-firstname")).sendKeys("Soumali");
+		driver.findElement(By.id("input-lastname")).sendKeys("Mukhopadhyay");
+		driver.findElement(By.id("input-email")).sendKeys(generateNewEmail());
+		driver.findElement(By.id("input-telephone")).sendKeys("1234567890");
+		driver.findElement(By.id("input-password")).sendKeys("12345");
+		driver.findElement(By.id("input-confirm")).sendKeys("12345");
+		driver.findElement(By.xpath("//input[@name='newsletter'][@value='1']")).click();
+		driver.findElement(By.name("agree")).click();
+		driver.findElement(By.xpath("//input[@value='Continue']")).click();
+		
+		Assert.assertTrue(driver.findElement(By.linkText("Logout")).isDisplayed());
+		Assert.assertTrue(driver.findElement(By.xpath("//ul[@class='breadcrumb']//a[text()='Success']")).isDisplayed());
+		
+		String expectedProperDetailsOne = "Your Account Has Been Created";
+		String expectedProperDetailsTwo = "Congratulations! Your new account has been successfully created!";
+		String expectedProperDetailsThree = "You can now take advantage of member privileges to enhance your online shopping experience with us.";
+		String expectedProperDetailsFour = "If you have ANY questions about the operation of this online shop, please e-mail the store owner.";
+		String expectedProperDetailsFive = "A confirmation has been sent to the provided e-mail address. If you have not received it within the hour, please contact us.";
+		
+		String actualProperDetails = driver.findElement(By.id("content")).getText();
+		
+		Assert.assertTrue(actualProperDetails.contains(expectedProperDetailsOne));
+		Assert.assertTrue(actualProperDetails.contains(expectedProperDetailsTwo));
+		Assert.assertTrue(actualProperDetails.contains(expectedProperDetailsThree));
+		Assert.assertTrue(actualProperDetails.contains(expectedProperDetailsFour));
+		Assert.assertTrue(actualProperDetails.contains(expectedProperDetailsFive));
+		
+		driver.findElement(By.linkText("Continue")).click();
+		
+		Assert.assertTrue(driver.findElement(By.linkText("Edit your account information")).isDisplayed());
+		
+		driver.quit();
+	}
+	
+	public static String generateNewEmail() {
+		Date date = new Date();
+		//String stringDate = date.toString();
+		//String noSpaceStringDate = date.toString().replaceAll("\\s","");
+		//String noColonStringDate = date.toString().replaceAll("\\s","").replaceAll("\\:","");
+		return date.toString().replaceAll("\\s","").replaceAll("\\:","")+"@gmail.com";
+		//System.out.println(emailWithTimeStamp);
+	}
+
+}
